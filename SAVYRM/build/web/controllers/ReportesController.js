65a2e7525d32ba58b3da8d93ff6000d4cd1190a4
unknown <Ted.Orellana@Belltech.la>
2020-11-06 01:49:49 -0500

    Reporte de productos vendidos terminado

--- SAVYRM/build/web/controllers/ReportesController.js
+++ SAVYRM/build/web/controllers/ReportesController.js
@@ -9,58 +9,43 @@ angular.module('angularRoutingApp').controller('reportesController', function ($
     // $scope.PrecioTotal = "00.00";
     // $scope.shoppingCarEmpty = true; // used to hide table with car items when is empty
     
-    // // Obtiene productos para la venta
-    // $scope.registrarVenta = function(){
-    //     var detallesServicio = {
-    //         idEmpleado : $sessionStorage.currentUser,
-    //         idCliente : $sessionStorage.currentUser, // TODO: pending to add customer.
-    //         dateTimeServiceBegin : begginningDateTime
-    //     }
-        
-    //     alert("registrando venta ");
-    //     $http({
-    //         method: 'POST',
-    //         url: 'http://localhost:8080/Venta/RegistrarVenta',
-    //         data: { 
-    //             carritoDeCompras,
-    //             detallesServicio
-    //         }
-    //     }).then(function successCallback(response) {
-    //         alert("Venta realizada!.");
-    //         carritoDeCompras = null;
-    //         $scope.carrito = carritoDeCompras;
-    //         }, function errorCallback(response) {
-    //         //alert("Ups! Ocurrio un error. Por favor, inténtalo más tarde.");
-    //     });     
-    // };
+    // Obtiene productos para la venta
+    $scope.GetAllSales = function(){
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/Report/GetAllSales',
+            data: { }
+        }).then(function successCallback(response) {
+            $scope.PopulateSalesReport(response.data);
+            }, function errorCallback(response) {
+            alert("Ups! Ocurrio un error. Por favor, inténtalo más tarde.");
+        });
+    };
 
-    $scope.GetSalesReport = function(){
+    $scope.PopulateSalesReport = function(arraydata){
         // Construct options first and then pass it as a parameter
+        var tableData =[];
+
+        // prepare data to be show in the graphic
+        for (var i = 0; i < arraydata.length ;i++) {
+            var element = {
+                label : arraydata[i].nombreProducto,
+                y: arraydata[i].cantidadTotal
+            }
+            tableData.push(element);
+        }
+        
+
         var options1 = {
             animationEnabled: true,
             title: {
-                text: "Chart inside a jQuery Resizable Element"
+                text: "Producto vendidos"
             },
             data: [{
                 type: "column", //change it to line, area, bar, pie, etc
-                legendText: "Try Resizing with the handle to the bottom right",
+                legendText: "Cantida por producto",
                 showInLegend: true,
-                dataPoints: [
-                    { label: "Iraq", y: 100 },
-                    { y: 600 },
-                    { y: 400 },
-                    { y: 120 },
-                    { y: 190 },
-                    { y: 300 },
-                    { y: 900 },
-                    { y: 10 },
-                    { y: 10 },
-                    { y: 10 },
-                    { y: 10 },
-                    { y: 10 },
-                    { y: 22 },
-                    { y: 400 }
-                    ]
+                dataPoints: tableData
                 }]
         };
     
